59255e57442cf476a4cc27fffe24b9c3dd0da697
theSovereignProject <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-11-02 14:57:48 +0530

    adding auth using clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,10 @@ import { ArrowRightIcon, MoveRightIcon } from "lucide-react"
 import Link from "next/link"
 import { MaxWidthWrapper } from "./MaxWidthWrapper"
 import { Button, buttonVariants } from "./ui/button"
+import { currentUser } from "@clerk/nextjs/server"
 
-export const Navbar = () => {
-  const user = false
+export const Navbar = async () => {
+  const user = await currentUser()
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
